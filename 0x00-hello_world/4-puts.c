1f2780fb21ae0d3b3310e9f0ab363af342106206
“mouradmath1” <mouradmathstem2@gmail.com>
2023-03-17 04:44:39 -0700

    t

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
-# include<stdio.h>
+#include <stdio.h>
+
 /**
- * test
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
- int main(void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
