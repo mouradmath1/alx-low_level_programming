5473a6b4a53231beb326ed958e860191d3e6491a
“mouradmath1” <mouradmathstem2@gmail.com>
2023-03-19 12:24:21 -0700

    t

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,8 @@
  */
 int main(void)
 {    
-               fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 1, 59, stderr);
-                return (0);
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 1, 59, stderr);
+	return(1) ;
+             
+
 }
