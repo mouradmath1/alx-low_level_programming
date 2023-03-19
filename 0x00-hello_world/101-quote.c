f04e0af2e509fca090c4116654bd28a2dd2443a6
“mouradmath1” <mouradmathstem2@gmail.com>
2023-03-19 11:54:03 -0700

    t

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
-{
-    fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 1, 59, stderr);
-   return(1);
+{    
+               fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 1, 59, stderr);
+                return (0);
 }
